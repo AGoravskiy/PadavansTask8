7bea2e07fbc03ce14aeced6445f290b9d4d1cf64
Aleksey <Narchuk.Aleksey@gmail.com>
2019-02-24 01:31:45 +0300

    initisl

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System;
+using System.Collections.Generic;
 
 namespace PadawansTask8
 {
@@ -7,8 +8,45 @@ namespace PadawansTask8
     {
         public static void RemoveDuplicateWords(ref string text)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (text == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (text == "")
+            {
+                throw new ArgumentException();
+            }
+            string word = "";
+            List<string> words = new List<string>();
+            List<char> symbols = new List<char> { '.', ',', '!', '?', '-', ':', ';', ' ' };
+            int beginPosition = 0;
+            for (int i = 0; i <= text.Length; i++)
+            {
+                if (i == text.Length || symbols.Contains(text[i]))
+                {
+                    if (word != "")
+                    {
+                        if (words.Contains(word))
+                        {
+                            text = text.Remove(beginPosition + 1, word.Length);
+                            i -= word.Length;
+                        }
+                        else
+                        {
+                            words.Add(word);
+                        }
+                        beginPosition = i;
+                        word = "";
+                    }
+                }
+                else
+                {
+                    word += text[i];
+                }
+            }
         }
     }
+
+
+    
 }
